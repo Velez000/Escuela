05fb0c40ca53678f2a7b7c4f48b41d036d4bdaa6
Velez000 <trabajosvelez@gmail.com>
2021-09-08 11:48:58 -0500

    Aprendiz(Home and rating)

---
+++ js/index.js
@@ -0,0 +1,47 @@
+var open = document.querySelector('.buttom-open');
+var container = document.querySelector('.div-container');
+var close = document.querySelector('.buttom-close');
+var open2 = document.querySelector('.buttom-open2');
+var container2 = document.querySelector('.container2');
+var close2 = document.querySelector('.buttom-close2');
+var open3 = document.querySelector('.buttom-open3');
+var container3 = document.querySelector('.container3');
+var close3 = document.querySelector('.buttom-close3');
+var open4 = document.querySelector('.buttom-open4');
+var container4 = document.querySelector('.container4');
+var close4 = document.querySelector('.buttom-close4');
+
+    open.addEventListener('click', ()=>{
+        container.style.display = "block";
+        console.log('presiono el container 1')
+    });
+
+    close.addEventListener('click', ()=>{
+        container.style.display = "none";
+    });
+
+    open2.addEventListener('click', ()=>{
+        container2.style.display = "block";
+        console.log('presiono el container 2')
+    });
+
+    close2.addEventListener('click', ()=>{
+        container2.style.display = "none";
+    });
+    open3.addEventListener('click', ()=>{
+        container3.style.display = "block";
+        console.log('presiono el container 1')
+    });
+
+    close3.addEventListener('click', ()=>{
+        container3.style.display = "none";
+    });
+
+    open4.addEventListener('click', ()=>{
+        container4.style.display = "block";
+        console.log('presiono el container 2')
+    });
+
+    close4.addEventListener('click', ()=>{
+        container4.style.display = "none";
+    });
